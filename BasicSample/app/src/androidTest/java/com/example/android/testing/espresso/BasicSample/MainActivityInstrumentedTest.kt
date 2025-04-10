d072cb1731a3a13c9d1861002ddfe5edef0430ff
harsh <harshbpatel0909@gmail.com>
2025-04-10 00:48:57 -0400

    Add Care for Instumatal test

--- BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
+++ BasicSample/app/src/androidTest/java/com/example/android/testing/espresso/BasicSample/MainActivityInstrumentedTest.kt
@@ -1,80 +1,44 @@
 package com.example.android.testing.espresso.BasicSample
 
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@LargeTest
 class MainActivityInstrumentedTest {
 
     @get:Rule
-    @JvmField
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    var activityScenarioRule = activityScenarioRule<MainActivity>()
 
     private fun enterText(input: String) {
-        Espresso.onView(withId(R.id.editTextUserInput))
-            .perform(ViewActions.typeText(input), ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.editTextUserInput))
+            .perform(replaceText(input), closeSoftKeyboard())
     }
 
     @Test
     fun testChangeText_with123Input() {
         enterText("123")
-        Espresso.onView(withId(R.id.changeTextBt)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.textToBeChanged))
-            .check(ViewAssertions.matches(ViewMatchers.withText("123")))
+        onView(withId(R.id.changeTextBt)).perform(click())
+        onView(withId(R.id.textToBeChanged))
+            .check(matches(withText("123")))
     }
 
     @Test
     fun testChangeText_withEmptyInput() {
         enterText("")
-        Espresso.onView(withId(R.id.changeTextBt)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.textToBeChanged))
-            .check(ViewAssertions.matches(ViewMatchers.withText("")))
+        onView(withId(R.id.changeTextBt)).perform(click())
+        onView(withId(R.id.textToBeChanged))
+            .check(matches(withText("")))
     }
 
-    @Test
-    fun testChangeText_withAbcdefInput() {
-        enterText("abcdef")
-        Espresso.onView(withId(R.id.changeTextBt)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.textToBeChanged))
-            .check(ViewAssertions.matches(ViewMatchers.withText("abcdef")))
-    }
+    //here
 
-    @Test
-    fun testOpenActivity_with123Input() {
-        enterText("123")
-        Espresso.onView(withId(R.id.activityChangeTextBtn)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.show_text_view))
-            .check(ViewAssertions.matches(ViewMatchers.withText("123")))
-    }
-
-    @Test
-    fun testOpenActivity_withEmptyInput() {
-        enterText("")
-        Espresso.onView(withId(R.id.activityChangeTextBtn)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.show_text_view))
-            .check(ViewAssertions.matches(ViewMatchers.withText("")))
-    }
-
-    @Test
-    fun testOpenActivity_withNoInput() {
-        Espresso.onView(withId(R.id.activityChangeTextBtn)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.show_text_view))
-            .check(ViewAssertions.matches(ViewMatchers.withText("")))
-    }
-
-    @Test
-    fun testOpenActivity_withAbcdefInput() {
-        enterText("abcdef")
-        Espresso.onView(withId(R.id.activityChangeTextBtn)).perform(ViewActions.click())
-        Espresso.onView(withId(R.id.show_text_view))
-            .check(ViewAssertions.matches(ViewMatchers.withText("abcdef")))
-    }
 }
